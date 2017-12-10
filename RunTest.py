7b0af0f333f6c714cedc16f94fecb674cb803866
Linozeross <lino@toranjenner.de>
2017-12-09 20:56:41 -0500

    using sparse matrix, using softmax

--- RunTest.py
+++ RunTest.py
@@ -110,7 +110,7 @@ print(model.summary())
 
 seq = ToDenseSeq(ag_train_input,Y_train,32)
 
-model.fit_generator(seq,steps_per_epoch=3750, epochs=10, verbose=1)
+model.fit_generator(seq,steps_per_epoch=3750, epochs=5, verbose=1)
 
 seq = ToDenseSeq(ag_test_input,Y_test,400)
 print(model.evaluate_generator(seq,steps=19))
