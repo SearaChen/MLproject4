b67e6025ea3a261b3369492b3513fe7f143b07fb
Linozeross <lino@toranjenner.de>
2017-12-13 01:32:45 -0500

    using sparse matrix, using softmax

--- RunTest.py
+++ RunTest.py
@@ -60,7 +60,11 @@ if __name__ == '__main__':
 
     ag_train_labels = ag_train_data[:,0]
     #concatenate title and description
-    ag_train_text = [' '.join(s) for s in zip(ag_train_data[:,1], ag_train_data[:,2])]
+
+    if arg == 'yelp_pol':
+        ag_train_text = ag_train_data[:,1]
+    else:
+        ag_train_text = [' '.join(s) for s in zip(ag_train_data[:,1], ag_train_data[:,2])]
     #print(np.shape(ag_train_text))
     #ag_train_text = np.asarray(ag_train_text)
 
